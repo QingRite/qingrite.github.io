85e281367ca8f8924ca3cca667069c88c1c2aaf5
Qing Rong <chinqr20@gmail.com>
2024-01-19 05:03:50 +0800

    added more animations (hover, project status bar)

--- project-scrollanimations.js
+++ project-scrollanimations.js
@@ -1,24 +1,24 @@
 //function for handling animations
 // stores as skill proficiency bar elements as array
-export const skillprof = Array.from(document.getElementsByClassName("skill-prof"));
-export const proftext = Array.from(document.getElementsByClassName("prof-percent"))
+export const statusbar = Array.from(document.getElementsByClassName("statusbar"));
+export const statustext = Array.from(document.getElementsByClassName("statustext"))
 //set current width to 0    
 export var index = 0;
-export var skillprofvalue = skillprof[index];
-export var originalWidth = skillprofvalue.offsetWidth;
+export var statusbarvalue = statusbar[index];
+export var originalWidth = statusbarvalue.offsetWidth;
 export var widthData = []
 
 //function for handling resize
 export function handleResize() {
-    var originalWidth = skillprofvalue.offsetWidth; 
-    console.log(skillprofvalue.offsetWidth);
-    skillprofvalue.style.width = originalWidth + 'px';
+    var originalWidth = statusbarvalue.offsetWidth; 
+    console.log(statusbarvalue.offsetWidth);
+    statusbarvalue.style.width = originalWidth + 'px';
     console.log('resize')
 }
 
 //Function that Check if element is in viewport
 export function isInViewport() {
-    var elmt = skillprofvalue.getBoundingClientRect();
+    var elmt = statusbarvalue.getBoundingClientRect();
     //variable that returns bottom edge of current viewport
     var bottomY = window.scrollY + window.innerHeight;
     return (elmt.top >= 0 && bottomY < elmt.bottom);
@@ -26,22 +26,22 @@ export function isInViewport() {
 
 //function that returns elements to original size for animations\
 export function returnToOriginalSize() {
-    var skillprofvalue = skillprof[index];  
+    var statusbarvalue = statusbar[index];  
     console.log(widthData[index]);
-    skillprofvalue.style.width = widthData[index] + 'px';
-    return (skillprofvalue.style.width);
+    statusbarvalue.style.width = widthData[index] + 'px';
+    return (statusbarvalue.style.width);
 }
 
 //function that fades in proficiency percentage text after returning width to original size
 export function fadeText() {
-    proftext[index].style.opacity = '1.0';
+    statustext[index].style.opacity = '1.0';
 }
 
 //function that handles scroll animations
 //function calls itself until end of index
 export function scrollHandler() {
-    if (index <= skillprof.length) {
-        if (isInViewport(skillprofvalue) == false ) {
+    if (index <= statusbar.length) {
+        if (isInViewport(statusbarvalue) == false ) {
             isInViewport();
             returnToOriginalSize();
             fadeText();
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-import * as scrollanimations from  "./scrollanimations.js";
+import * as skillscrollanimations from './skill-scrollanimations.js';
+import * as projectscrollanimations from './project-scrollanimations.js'
 import * as switchabletabs from './switchable-tabs.js';
 
 
@@ -6,22 +7,40 @@ import * as switchabletabs from './switchable-tabs.js';
 switchabletabs.button1.addEventListener('click', switchabletabs.clickHandler1);
 switchabletabs.button2.addEventListener('click', switchabletabs.clickHandler2);
 
-for (var i = 0; i < scrollanimations.skillprof.length; i++) {
+for (var i = 0; i < skillscrollanimations.skillprof.length; i++) {
     //copy each bar width 
-    var barWidth = scrollanimations.skillprof[i].offsetWidth
+    var barWidth = skillscrollanimations.skillprof[i].offsetWidth
     //push bar width data into an array
-    scrollanimations.widthData.push(barWidth);
+    skillscrollanimations.widthData.push(barWidth);
     //set all skill proficiency bar width to 0
-    scrollanimations.skillprof[i].style.width = 0 + 'px';
+    skillscrollanimations.skillprof[i].style.width = 0 + 'px';
 } 
 
 //hide all skill proficiency values
-for (var j = 0; j < scrollanimations.proftext.length; j++) {
-    scrollanimations.proftext[j].style.opacity = '0';
+for (var j = 0; j < skillscrollanimations.proftext.length; j++) {
+    skillscrollanimations.proftext[j].style.opacity = '0';
 }
 
-window.addEventListener('resize', scrollanimations.handleResize);
-document.addEventListener('DOMContentLoaded', scrollanimations.scrollHandler);
-setInterval(scrollanimations.scrollHandler, 100);
+for (var i = 0; i < projectscrollanimations.statusbar.length; i++) {
+    //copy each bar width 
+    var barWidth = projectscrollanimations.statusbar[i].offsetWidth
+    //push bar width data into an array
+    projectscrollanimations.widthData.push(barWidth);
+    //set all skill proficiency bar width to 0
+    projectscrollanimations.statusbar[i].style.width = 0 + 'px';
+} 
 
-scrollanimations.skillprof.forEach(scrollanimations.scrollHandler);   
+//hide all skill proficiency values
+for (var j = 0; j < projectscrollanimations.statustext.length; j++) {
+    projectscrollanimations.statustext[j].style.opacity = '0';
+}
+
+window.addEventListener('resize', skillscrollanimations.handleResize);
+window.addEventListener('resize', projectscrollanimations.handleResize);
+document.addEventListener('DOMContentLoaded', skillscrollanimations.scrollHandler);
+document.addEventListener('DOMContentLoaded', projectscrollanimations.scrollHandler);
+setInterval(projectscrollanimations.scrollHandler, 100);
+setInterval(skillscrollanimations.scrollHandler, 100);
+
+skillscrollanimations.skillprof.forEach(skillscrollanimations.scrollHandler);
+projectscrollanimations.skillprof.forEach(projectscrollanimations.scrollHandler);   
